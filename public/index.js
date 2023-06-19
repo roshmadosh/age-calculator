19909e1c374e51141ba6449af8cf1cd26ce97fac
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-06-18 20:06:16 -0400

    everything except mobile

--- public/index.js
+++ public/index.js
@@ -4,8 +4,16 @@ const monthsTens = document.getElementById('months-display-tens');
 const monthsOnes = document.getElementById('months-display-ones');
 const daysTens = document.getElementById('days-display-tens');
 const daysOnes = document.getElementById('days-display-ones');
+const inputsList = document.getElementsByClassName('input');
 
 const displayValues = [yearsTens, yearsOnes, monthsTens, monthsOnes, daysTens, daysOnes];
+
+let currentYearsTens;
+let currentYearsOnes;
+let currentMonthsTens;
+let currentMonthsOnes;
+let currentDaysTens;
+let currentDaysOnes;
 
 for (let val of displayValues) {
     for (i = 0; i < 10; i++) {
@@ -13,42 +21,71 @@ for (let val of displayValues) {
     }
 }
 
-
 function onSubmit(e) {
     e.preventDefault();
+    let isError = false;
     const monthInput = document.getElementById('month-input').value;
     const dayInput = document.getElementById('day-input').value;
     const yearInput = document.getElementById('year-input').value;
-
-    const today = new Date();
-    let diff = today - new Date(yearInput, monthInput, dayInput); 
-
-    if (diff < 0) {
-        alert('Birthday cannot be in the future');
-        return;
-    }
+    const today = moment();
 
     if (monthInput < 1 || monthInput > 12) {
-        alert('Month invalid');
-        return;
+        inputsList[0].classList.add('invalid');
+        isError = true;
+    } else {
+        inputsList[0].classList.remove('invalid');
     }
-    if (dayInput < 1 || monthInput > 31) {
-        alert('Day invalid');
-        return;
+
+    if (dayInput < 1 || dayInput > 31) {
+        inputsList[1].classList.add('invalid');
+        isError = true;
+    } else {
+        inputsList[1].classList.remove('invalid');
     }
-    if (yearInput < 1910 || yearInput > 2023) {
-        alert('Year invalid');
+    if (yearInput < today.years() - 99 || yearInput > today.years()) {
+        inputsList[2].classList.add('invalid');
+        isError = true;
+    } else {
+        inputsList[2].classList.remove('invalid');
+    }
+
+    const bday = moment(`${yearInput}-${monthInput}-${dayInput}`);
+    let diff = moment.preciseDiff(today, bday, true); 
+    if (!diff.firstDateWasLater) {
+        for (const input of inputsList) {
+            input.classList.add('invalid');
+            isError = true;
+        }
+    }
+     
+    if (isError) {
         return;
     }
 
-    const ageYears = Math.floor(diff / (1000 * 60 * 60 * 24 * 365));
-    diff -= ageYears * (1000 * 60 * 60 * 24 * 365);
-    const ageMonths = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
-    diff -= ageMonths * (1000 * 60 * 60 * 24 * 30);
-    const ageDays = Math.floor(diff / (1000 * 60 * 60 * 24));
-    diff -= ageDays * (1000 * 60 * 60 * 24);
+    const ageYears = diff.years < 10 ? '0'.concat(diff.years.toString()) : diff.years.toString();
+    const ageMonths = diff.months < 10 ? '0'.concat(diff.months.toString()) : diff.months.toString();
+    const ageDays = diff.days < 10 ? '0'.concat(diff.days.toString()) : diff.days.toString();
 
-    yearsTens.classList.add(`move-${ageYears.toString().charAt(0)}`);
+    yearsTens.classList.remove(currentYearsTens);
+    yearsOnes.classList.remove(currentYearsOnes);
+    monthsTens.classList.remove(currentMonthsTens);
+    monthsOnes.classList.remove(currentMonthsOnes);
+    daysTens.classList.remove(currentDaysTens);
+    daysOnes.classList.remove(currentDaysOnes);
+
+    currentYearsTens = `move-${ageYears.charAt(0)}`;
+    currentYearsOnes = `move-${ageYears.charAt(1)}`;
+    currentMonthsTens = `move-${ageMonths.charAt(0)}`;
+    currentMonthsOnes = `move-${ageMonths.charAt(1)}`;
+    currentDaysTens = `move-${ageDays.charAt(0)}`;
+    currentDaysOnes = `move-${ageDays.charAt(1)}`;
+
+    yearsTens.classList.add(currentYearsTens);
+    yearsOnes.classList.add(currentYearsOnes);
+    monthsTens.classList.add(currentMonthsTens);
+    monthsOnes.classList.add(currentMonthsOnes);
+    daysTens.classList.add(currentDaysTens);
+    daysOnes.classList.add(currentDaysOnes);
 
 }
 
